21032f98519d610ad8e870ddf68430a3bb7cdfa6
Alexandr <alex8.ivanov@gmail.com>
2021-04-18 22:32:00 +0300

    Fix: git conflict shit

--- anime-motivator/extension.js
+++ anime-motivator/extension.js
@@ -1,28 +1,17 @@
 
 const vscode = require('vscode');
 
-<<<<<<< HEAD
 const {Motivator} = require('./motivator')
-=======
->>>>>>> a1ce8a6... Chore: project init
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-<<<<<<< HEAD
 	console.log('Anime motivator is now active.');
 	
 	var motivator = new Motivator();
 
 	motivator.initialize();
-
-=======
-	console.log('Congratulations, your extension "anime-motivator" is now active!');
-
-	vscode.window.showInformationMessage("Hello world!.")
->>>>>>> a1ce8a6... Chore: project init
 }
 
 function deactivate() {}
